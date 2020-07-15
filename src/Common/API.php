3012ecc0c812eab5766138b7652ef59913264bd5
Mike iLL Kilmer <mike@mzoo.org>
2020-07-15 10:03:41 -0500

    Abstract API interface into separate class.

---
+++ src/Common/API.php
@@ -0,0 +1,78 @@
+<?php
+namespace MZ_Mobilize_America\Common;
+
+class API {
+
+    /*
+     * Basic Restful Request
+     */
+    private function callApi($method, $endpoint, $data = false) {
+
+        $curl = curl_init();
+        
+        $subdomain = 'staging-api';
+        
+        $url = 'https://' . $subdomain . '.mobilize.us/v1/' . $endpoint;
+        
+        $url = htmlentities($url);
+
+        switch ($method)
+        {
+            case "POST":
+                curl_setopt($curl, CURLOPT_POST, 1);
+
+                if ($data)
+                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+                break;
+            case "PUT":
+                curl_setopt($curl, CURLOPT_PUT, 1);
+                break;
+            default:
+                if ($data)
+                    $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+
+        // Optional Authentication:
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        // curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $result = curl_exec($curl);
+
+        curl_close($curl);
+
+        return $result;
+    }
+    
+    /*
+     * Make Request
+     *
+     * @since 1.0.0
+     * This is the static function through which this class is interfaced.
+     * @param $method string GET, POST
+     * @param $endpoint string 
+     * @param $method string 
+     */
+    public static function make_request($method, $endpoint, $data = false) {
+    
+        $response = self::callApi($method, $endpoint, $data);
+        
+        $result = json_decode($response);
+        
+        if (!empty($result->error)) {
+            return array('API Error' => $result->error);
+              $to = get_option('admin_email');
+              $subject = __('Mobilize America API Error', 'mobilize-america');
+              $message = __("There was an error returning events:", 'mobilize-america') . "\n" . print_r($result->error);
+              wp_mail( $to, $subject,  $message, '', $attachments );
+        } else if (!$result->count >= 1) {
+            return array('zero' => 1);
+        }
+
+        return json_decode($response)->data;
+    }
+}
+
+?>
--- src/Organizations/Organizations.php
+++ src/Organizations/Organizations.php
@@ -3,6 +3,7 @@ namespace MZ_Mobilize_America\Organizations;
 
 use MZ_Mobilize_America\ShortCode as ShortCode;
 use MZ_Mobilize_America as NS;
+use MZ_Mobilize_America\Common as Common;
 
 class Organizations extends ShortCode\ShortCode_Script_Loader {
 
@@ -44,81 +45,17 @@ class Organizations extends ShortCode\ShortCode_Script_Loader {
     }
 
     /*
-     * Basic Restful Request
-     */
-    private function CallAPI($method, $url, $data = false) {
-
-        $curl = curl_init();
-
-        switch ($method)
-        {
-            case "POST":
-                curl_setopt($curl, CURLOPT_POST, 1);
-
-                if ($data)
-                    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-                break;
-            case "PUT":
-                curl_setopt($curl, CURLOPT_PUT, 1);
-                break;
-            default:
-                if ($data)
-                    $url = sprintf("%s?%s", $url, http_build_query($data));
-        }
-
-        // Optional Authentication:
-        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        // curl_setopt($curl, CURLOPT_USERPWD, "username:password");
-
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-        $result = curl_exec($curl);
-
-        curl_close($curl);
-
-        return $result;
-    }
-
-    /*
      *
      *
      */
     private function retrieve_organizations($atts) {
 
 		$organization_id = $atts['organization_id'];
-        $now = new \DateTime(null, new \DateTimeZone('America/New_York'));
 
+        $endpoint = 'organizations';
 
-        // Allow One Day window to allow
-        // for in-progress events
-        $di = new \DateInterval('PT12H');
-        $di->invert = 1;
-        $now->add($di);
+        return Common\API::make_request('GET', $endpoint);
 
-        $events_start_time_filter = $now->getTimestamp();
-
-        $url_string = 'https://sandbox-api.mobilize.us/v1/organizations';
-            
-        $mobilize_url = htmlentities($url_string);
-
-        $response = $this->CallAPI('GET', $mobilize_url);
-
-        $result = json_decode($response);
-        
-        return $response;
-
-        if (!empty($result->error)) {
-            return array('API Error' => $result->error);
-              $to = get_option('admin_email');
-              $subject = __('Mobilize America API Error', 'mobilize-america');
-              $message = __("There was an error returning events:", 'mobilize-america') . "\n" . print_r($result->error);
-              wp_mail( $to, $subject,  $message, '', $attachments );
-        } else if (!$result->count >= 1) {
-            return array('zero' => 1);
-        }
-
-        return json_decode($response)->data;
     }
 
 }
