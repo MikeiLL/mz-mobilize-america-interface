324b09ffaf2461fc8caf803d005a87bceb130406
Mike iLL Kilmer <mike@mzoo.org>
2020-08-24 16:02:15 -0500

    Refine/debug zip script.

--- zipVersion.sh
+++ zipVersion.sh
@@ -4,23 +4,49 @@ DIRECTORY_TO_COMPRESS="mobilize-america"
 ZIPPED_FILE="mobilize-america.zip"
 
 cd ../
-echo "Changing directory and moving to new temp dir."
+echo "Change directory and move to new temp dir."
 mv mobilize-america mobilize-america-temp
-echo "Make temporary version of the plugin and copying desired files."
+echo "Make temporary version of the plugin and copy desired files."
 mkdir mobilize-america
 cp -r mobilize-america-temp/classes mobilize-america
 cp -r mobilize-america-temp/languages mobilize-america       
 cp -r mobilize-america-temp/src mobilize-america      
-cp -r mobilize-america-temp/vendor mobilize-america
-cp mz-mindbody-api-temp/*.php mobilize-america
-cp mz-mindbody-api-temp/README.txt mobilize-america
-cp mz-mindbody-api-temp/LICENSE.txt mobilize-america
-echo "Files copied. Making zip file."
+
+cp mobilize-america-temp/*.php mobilize-america
+cp mobilize-america-temp/README.txt mobilize-america
+cp mobilize-america-temp/LICENSE.txt mobilize-america
+cp mobilize-america-temp/composer.json mobilize-america
+
+echo "Move into Plugin Dir to install composer deps."
+cd mobilize-america 
+echo "Install production composer deps"
+# This requires jq: https://www.howtogeek.com/529219/how-to-parse-json-files-on-the-linux-command-line-with-jq/
+if [ -s './composer.json' ]; then
+    #Detect if there are composer dependencies
+    echo "-Check composer dependencies..."
+    if [ "$(uname)" == "Darwin" ]; then
+		dep=$(cat "./composer.json" | jq 'has("require")')
+	else
+		dep=$(cat "./composer.json" | jq 'has(".require")')
+	fi
+
+    if [ "$dep" == 'true' ]; then
+        echo "-Download clean composer dependencies..."
+        composer update --no-dev # &> /dev/null
+        echo "-Run composer dumpautoload -o"
+        composer dumpautoload -o
+    else
+        rm -rf ./composer.json
+    fi
+fi 
+echo "Move back up a level"
+cd ../
+echo "Make zip file."
 zip -r "$ZIPPED_FILE" "$DIRECTORY_TO_COMPRESS"
 echo $DIRECTORY_TO_COMPRESS "compressed as" $ZIPPED_FILE > /dev/null
-echo "Removing temp file and changing directories."
-rm -r "$DIRECTORY_TO_COMPRESS"
-echo "Renaming to original directory name."
+echo "Remove temp file and changing directories."
+rm -rf "$DIRECTORY_TO_COMPRESS"
+echo "Rename to original directory name."
 mv mobilize-america-temp mobilize-america
 cd mobilize-america
 echo "Zip file made and back home again."
