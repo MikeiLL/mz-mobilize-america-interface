72acd619f6db4faab63e783cd7ce46f3f3a259ba
Mike iLL Kilmer <mike@mzoo.org>
2020-08-26 18:13:05 -0500

    Do not display the thumbnail if shortcode specifies not to.

--- src/Frontend/views/events.php
+++ src/Frontend/views/events.php
@@ -16,7 +16,7 @@ use MZ_Mobilize_America\Libraries as Libraries;
 foreach($data->api_object->request_results->data as $k => $event){ ?>
     <h5><?php echo $event->title; ?></h5>
     <p>
-    <?php if (!empty($event->featured_image_url)): ?>
+    <?php if ( ($data->atts['thumbnail'] != false) && !empty($event->featured_image_url) ): ?>
     <img src="<?php echo $event->featured_image_url; ?>" class="mobilize-event-image alignright">
     <?php endif; ?>
     <?php echo $event->description; ?>
