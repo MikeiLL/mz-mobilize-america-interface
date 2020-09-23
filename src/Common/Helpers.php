5d6566345b8655d3e2ceeb4bc84894fc4a1189b4
Mike iLL Kilmer <mike@mzoo.org>
2020-09-23 10:11:56 -0500

    Add a log and print method for dev.

--- src/Common/Helpers.php
+++ src/Common/Helpers.php
@@ -34,5 +34,51 @@ class Helpers {
         $vars[] = "mobilize_page";
         return $vars;
     }
+    
+    
+    /**
+     * Helper function to write strings or arrays to the screen
+     *
+     * @since     1.0.1
+     *
+     * @param $message the content to be written to screen.
+     */
+    public function print($message = '')
+    {
+        echo "<pre>";
+        print_r($message);
+        echo "</pre>";
+    }
+
+    /**
+     * Helper function to write strings or arrays to a file
+     *
+     * @since     1.0.1
+     *
+     * @param $message the content to be written to file.
+     * @param $file_path string optional path to write file to.
+     */
+    public function log($message, $file_path='')
+    {
+        $file_path = ( ($file_path == '') || !file_exists($file_path) ) ? WP_CONTENT_DIR . '/mobilize_america.log' : $file_path;
+        $header = date('l dS \o\f F Y h:i:s A', strtotime("now")) . "\t ";
+
+        // Just keep up to seven days worth of data
+        if (file_exists($file_path)){
+            if (time() - filemtime($file_path) >= 60 * 60 * 24 * 7) { // 7 days
+                unlink($file_path);
+            }
+        }
+
+        if (is_array($message) || is_object($message)) {
+            $message = print_r($message, true);
+        }
+        $message .= "\n";
+        file_put_contents(
+            $file_path,
+            $header . $message,
+            FILE_APPEND | LOCK_EX
+        );
+    }
 }
 ?>
